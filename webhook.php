207196b7457c30a63c8399f8ac9d08410a5a1e50
succos <3322480003@qq.com>
2019-01-18 17:25:30 +0800

    2

---
+++ webhook.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$secret="miyao";
+
+$path="/www/wwwroot/succos";
+
+$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+if($signature){
+    $hash="sha1=".hash_hmac('sha1',file_get_contents("php://input"),$secret);
+    if(strcmp($signature,$hash)==0){
+        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        exit();
+    }
+}
+http_response_code(404);
